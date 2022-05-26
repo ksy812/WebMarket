3afd4231f5e6df0249f2c5ee77b839eaeb859f72
Kim So Yeon <s2023@e-mirim.hs.kr>
2022-05-27 08:21:23 +0900

    Add proxy for Open API Access-Control-Allow-Origin 이슈 해결 위한 코드 추가

--- app.js
+++ app.js
@@ -1,27 +1,53 @@
+$(document).ready(function () {
+    console.log("ready");
+    $("#search").on("click", function () {
+        alert("click");
+        $.ajax({
+            method: "GET",
+            url: "http://openapi.11st.co.kr/openapi/OpenApiService.tmall?key=d31cb5254083f025e9231e22960e7e14&apiCode=ProductSearch&keyword=xnote",
+            data: {}
+        }).done(function (msg) {
+            console.log("test: " + msg);
+            $("p").append(msg);
+        });
+    })
+})
 
 const   express = require("express"),
         http = require("http"),
         cors = require("cors"),
         path = require("path"),
-        $ = require("jquery"),
+        //$ = require("jquery")(require("jsdom").jsdom().parentWindow),
         request = require("request"),
         iconv = require("iconv-lite");
-// const {JSDOM} = require("jsdom");
-// const {window} = new JSDOM("");
-// const $ = require("jquery")(window);
 
 const router = express.Router();
 const app = express();
 app.set("port", process.env.PORT || 3000);
 app.use("/", router);
+
 app.use(cors());
+app.route("/ajax");
 app.use("/node_modules", express.static(path.join(__dirname, "/node_modules")));
 
 
 const key = "d31cb5254083f025e9231e22960e7e14";
 var keyword = "케이블";
 
-
+// $(document).ready(function(){
+//     console.log("ready");
+//     $(".search").on("click", function(){
+//         $.ajax({
+//             method: "GET",
+//             url: "http://openapi.11st.co.kr/openapi/OpenApiService.tmall?key=d31cb5254083f025e9231e22960e7e14&apiCode=ProductSearch&keyword=xnote",
+//             data: {}
+//         }).done(function(msg){
+//             console.log("test: "+ msg);
+//             $("p").append(msg);
+//         });
+//     })
+// })
+/*
 $.ajax({
     method: "GET",
     url: "http://openapi.11st.co.kr/openapi/OpenApiService.tmall?key="
@@ -35,9 +61,23 @@ $.ajax({
 }).done(function(msg){
     console.log("test: "+ msg);
 });
-
+*/
 router.route('/process/search').post(function(req, res) {
 	console.log('/process/search 호출됨.');
+    $.ajax({
+        method: "GET",
+        url: "http://openapi.11st.co.kr/openapi/OpenApiService.tmall?key="
+        + key +"&apiCode=ProductSearch&keyword="+ keyword,
+        data: {
+            // key: key,
+            // apiCode: "ProductSearch",
+            // keyword: keyword,
+            // pageNum: 1
+        }
+    }).done(function(msg){
+        console.log("test: "+ msg);
+        $("p").append(msg);
+    });
 });
 
 http.createServer(app).listen(app.get('port'), function(){
