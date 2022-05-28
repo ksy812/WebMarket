09c5ff09a373ff38720ecab00f9bcfbe0ff8a0ee
Kim So Yeon <s2023@e-mirim.hs.kr>
2022-05-28 17:16:54 +0900

    0528 백업

--- app.js
+++ app.js
@@ -15,55 +15,58 @@ app.use("/", router);
 app.use(cors());
 app.route("/ajax");
 app.use("/node_modules", express.static(path.join(__dirname, "/node_modules")));
-
-
+const document = "index.html";
 const key = "d31cb5254083f025e9231e22960e7e14";
-var keyword = "케이블";
+var keyword = "인터넷 케이블 랜선";
 
 $(document).ready(function () {
     console.log("ready");
-    $("#search").click(function () {
+    $("#search").on("click", function () {
         alert("click");
         $.ajax({
             method: "GET",
-            url: "https://cors-anywhere.herokuapp.com/http://openapi.11st.co.kr/openapi/OpenApiService.tmall?key=d31cb5254083f025e9231e22960e7e14&apiCode=ProductSearch&keyword=xnote",
+            datatype: "jsonp",
+            url: "https://cors-anywhere.herokuapp.com/http://openapi.11st.co.kr/openapi/OpenApiService.tmall?key=d31cb5254083f025e9231e22960e7e14&apiCode=ProductSearch&keyword="+keyword,
             data: {}
         }).done(function (msg) {
-            console.log("test: " + msg);
+            var str = msg.document["ProductSearchResponse"]["Products"]["Product"][0]["ProductName"];
             $("p").append(msg);
+            console.log(msg);
         });
-    })
-})
+let xhttp = new XMLHttpRequest();
 
-
-// $(document).ready(function(){
-//     console.log("ready");
-//     $(".search").on("click", function(){
-//         $.ajax({
-//             method: "GET",
-//             url: "http://openapi.11st.co.kr/openapi/OpenApiService.tmall?key=d31cb5254083f025e9231e22960e7e14&apiCode=ProductSearch&keyword=xnote",
-//             data: {}
-//         }).done(function(msg){
-//             console.log("test: "+ msg);
-//             $("p").append(msg);
-//         });
-//     })
-// })
-/*
-$.ajax({
-    method: "GET",
-    url: "http://openapi.11st.co.kr/openapi/OpenApiService.tmall?key="
-    + key +"&apiCode=ProductSearch&keyword="+ keyword,
-    data: {
-        // key: key,
-        // apiCode: "ProductSearch",
-        // keyword: keyword,
-        // pageNum: 1
+xhttp.onreadystatechange = function () {
+    
+    if(this.readyState == 4 && this.status == 200){
+    nodeValfunc( this ); // this == xhttp 
     }
-}).done(function(msg){
-    console.log("test: "+ msg);
-});
-*/
+}
+xhttp.open("GET", "https://cors-anywhere.herokuapp.com/http://openapi.11st.co.kr/openapi/OpenApiService.tmall?key=d31cb5254083f025e9231e22960e7e14&apiCode=ProductSearch&keyword=pen", true);
+xhttp.send();
+
+function nodeValfunc( xml ) { // ( xml ) 객체 넘겨받기
+    let num, name;
+    let txt, numtxt, xmlDoc;
+    
+    txt = numtxt = ""; // 빈 문자열로 초기화
+    
+    xmlDoc = xml.responseXML; 
+    
+    num = xmlDoc.getElementsByTagName("Product");
+    name = xmlDoc.getElementsByTagName("ProductName");
+    console.log(num.length);
+    
+    //for(i=0; i < num.length; i++){
+        txt += num[0].childNodes[0].nodeValue + "\n";
+        numtxt += name[0].childNodes[0].nodeValue + "\n";
+        console.log(txt + numtxt);
+    //}
+    $("p").append(txt + numtxt);
+    // 실행하면 번호와 이름이 p태그에 출력된다.
+}
+    })
+})  
+
 router.route('/process/search').post(function(req, res) {
 	console.log('/process/search 호출됨.');
     
