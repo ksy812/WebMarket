389db6058d450cfaa3b52d8006b4c7fc39b09892
Kim So Yeon <s2023@e-mirim.hs.kr>
2022-05-29 00:13:38 +0900

    검색 결과 단순 출력 이전 커밋의 문제는 https://cors-anywhere.herokuapp.com/데모 서버 신청하지 않은 문제였음.

--- app.js
+++ app.js
@@ -35,6 +35,10 @@ app.use(express.json());
 app.use(static(path.join(__dirname, 'public')));
 app.use("/node_modules", express.static(path.join(__dirname, "/node_modules")));
 
+
+
+
+
 router.route('/products').get(function (req, res) {
     console.log('/products 호출됨.');
     //searchKeyword = req.;
@@ -44,46 +48,6 @@ router.route('/products').get(function (req, res) {
     res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
     res.write( "<h1>/products 내용</h1>");
 	res.write("<p>왜 작동을 안할까??");
-
-    $(document).ready(function () {
-        console.alert("ready");
-        //console.log(searchKeyword);
-        //ajax
-        res.write("<p>ready() 후");
-        let xhttp = new XMLHttpRequest();
-
-        xhttp.onreadystatechange = function () {
-            
-            if(this.readyState == 4 && this.status == 200){
-            nodeValfunc( this ); // this == xhttp 
-            }
-        }
-        xhttp.open("GET", "https://cors-anywhere.herokuapp.com/http://openapi.11st.co.kr/openapi/OpenApiService.tmall?key=d31cb5254083f025e9231e22960e7e14&apiCode=ProductSearch&keyword="+searchKeyword, true);
-        xhttp.send();
-
-        function nodeValfunc( xml ) { // ( xml ) 객체 넘겨받기
-            let num, name;
-            let txt, numtxt, xmlDoc;
-            
-            txt = numtxt = ""; // 빈 문자열로 초기화
-            
-            xmlDoc = xml.responseXML; 
-            
-            num = xmlDoc.getElementsByTagName("Product");
-            name = xmlDoc.getElementsByTagName("ProductName");
-            console.log(num.length);
-            
-            //for(i=0; i < num.length; i++){
-                txt += num[0].childNodes[0].nodeValue + "\n";
-                numtxt += name[0].childNodes[0].nodeValue + "\n";
-                console.log(txt + numtxt);
-            //}
-            $("p").append(txt + numtxt);
-            console.log(txt + numtxt);
-        }
-
-    });
-    res.end();
 });
 
 http.createServer(app).listen(app.get('port'), function () {
