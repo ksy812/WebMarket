585324baa4ca3912622924c4cada81c0c6c56166
Kim So Yeon <s2023@e-mirim.hs.kr>
2022-05-29 17:10:12 +0900

    index.html 상품 상세설명 버튼 추가 app.js 실행 시 에러 발생함.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const apiKey = "d31cb5254083f025e9231e22960e7e14";
-var searchKeyword = "xnote";
+// const apiKey = "d31cb5254083f025e9231e22960e7e14";
+// var searchKeyword = "xnote";
 
 var express = require("express"),
     http = require("http"),
@@ -15,17 +15,17 @@ var express = require("express"),
     iconv = require("iconv-lite");
 const { fstat } = require("fs");
 
-const { JSDOM } = jsdom;
-const { window } = new JSDOM();
-const { document } = (new JSDOM("<!DOCTYPE html>index")).window;
-global.document = document;
-const $ = jQuery = require('jquery')(window);
+// const { JSDOM } = jsdom;
+// const { window } = new JSDOM();
+// const { document } = (new JSDOM("<!DOCTYPE html>index")).window;
+// global.document = document;
+// const $ = jQuery = require('jquery')(window);
 
 
 const router = express.Router();
 const app = express();
 app.set("port", process.env.PORT || 3000);
-app.route("/ajax");
+// app.route("/ajax");
 app.use(cors());
 app.use("/", router);
 app.use(express.urlencoded({ extended: false }));
@@ -33,22 +33,20 @@ app.use(express.json());
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended: false}));
 app.use(static(path.join(__dirname, 'public')));
-app.use("/node_modules", express.static(path.join(__dirname, "/node_modules")));
+// app.use("/node_modules", express.static(path.join(__dirname, "/node_modules")));
 
 
 
+// router.route('/products').get(function (req, res) {
+//     console.log('/products 호출됨.');
+//     //searchKeyword = req.;
+//     //searchKeyword = req.body.searchKeyword;
+//     //console.log(searchKeyword);
 
-
-router.route('/products').get(function (req, res) {
-    console.log('/products 호출됨.');
-    //searchKeyword = req.;
-    //searchKeyword = req.body.searchKeyword;
-    //console.log(searchKeyword);
-
-    res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-    res.write( "<h1>/products 내용</h1>");
-	res.write("<p>왜 작동을 안할까??");
-});
+//     res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+//     res.write( "<h1>/products 내용</h1>");
+// 	res.write("<p>왜 작동을 안할까??");
+// });
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
---
+++ products.js
@@ -0,0 +1,47 @@
+"use strict"; 
+const acaoUrl = "https://cors-anywhere.herokuapp.com/";
+const baseUrl = "http://openapi.11st.co.kr/openapi/OpenApiService.tmall";
+const apiKey = "d31cb5254083f025e9231e22960e7e14";
+let searchKeyword = "";//"xnote";
+
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+
+global.document = document;
+const $ = jQuery = require('jquery')(window);
+
+$(document).ready(function () {
+    console.log("ready");
+    $("#btnSearch").on("click", function () {
+        searchKeyword = document.getElementById("searchKeyword").value;
+        console.log(searchKeyword);
+        let url = acaoUrl + baseUrl
+            + "?key=" + apiKey
+            + "&apiCode=ProductSearch&keyword=" + searchKeyword;
+        let html = "";
+        $.ajax({
+            method: "GET",
+            datatype: "xml",
+            url: url,
+            success: function (xml) {
+                $(xml).find("Product").each(function () {
+                    let producUrl = $(this).find("DetailPageUrl").text();
+                    let productName = $(this).find("ProductName").text();
+                    let productImg =  $(this).find("ProductImage").text();
+                    let productPrice = $(this).find("ProductPrice").text();
+                    let productSeller = $(this).find("Seller").text();
+                    let productRating = $(this).find("Rating").text();
+                    html += "<li><div>" // onclick=location.href='" + producUrl + "'
+                        + "<img src="+productImg+" alt='상품 이미지'>"
+                        + "<h3>" + productName + "</h3>"
+                        + "<p>" + productPrice
+                        + "<p>" + productSeller
+                        + "<p>" + productRating
+                        + "<input type='button' value='상세설명' onClick='location.href=`"+ +producUrl+"`" +"'/>"
+                        + "</div></li>";
+                    $("#productList").html(html);
+                });
+            }
+        });
+    });
+});
