5a72e882541557f1a89eb6bed52c5575d5a0426e
Kim So Yeon <s2023@e-mirim.hs.kr>
2022-05-31 18:36:07 +0900

    상품 상세설명 기능 productInfo.js 내용을 express+ejs으로 변경함

--- app.js
+++ app.js
@@ -3,68 +3,76 @@ var express = require("express"),
     cors = require("cors"),
     path = require("path"),
     fs = require("fs"),
-    // bodyParser = require("body-parser"), //생략 가능
     static = require('serve-static'),
     expressErrorHandler = require('express-error-handler'),
-    //XMLHttpRequest  = require("xhr2"),
-    // jsdom = require('jsdom'),
-    // $ = require("jquery"), //(require("jsdom").jsdom().parentWindow),
-    request = require("request");
+    anxios = require("axios"),
+    request = require("request"),
+    ejs = require('ejs'),
+    iconv = require('iconv-lite'),
+    convert = require('xml-js'),
+    { default: axios } = require("axios");
+
+const router = express.Router();
+const app = express();
+
+//const acaoUrl = "https://cors-anywhere.herokuapp.com/";
+const baseUrl = "http://openapi.11st.co.kr/openapi/OpenApiService.tmall";
+const apiKey = "d31cb5254083f025e9231e22960e7e14";
 
 // var products = require("./products");
 // var productInfo = require("./productInfo");
 
-const router = express.Router();
-const app = express();
 app.set("port", process.env.PORT || 3000);
-app.route("/ajax");
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'html');
+
 app.use(cors());
 app.use("/", router);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(static(path.join(__dirname, 'public')));
-app.use("/node_modules", express.static(path.join(__dirname, "/node_modules")));
 
-// app.use("/products", products);
-// app.use("/productInfo", productInfo);
+app.engine('html', require('ejs').renderFile);
 
-app.get("/", function(req, res){
-    res.render("index", {title:"express"});
-    //res.sendFile(__dirname+'/index.html');
+app.get("/", function (req, res) {
+    res.render("index");
 })
 
-router.route("/products/:productCode").get(function (req, res) {
+router.route("/products/:productCode").get(async function (req, res) {
     console.log('/products/:productCode 호출됨.');
     let productCode = req.params.productCode;
-    const acaoUrl = "https://cors-anywhere.herokuapp.com/";
-    const baseUrl = "http://openapi.11st.co.kr/openapi/OpenApiService.tmall";
-    const apiKey = "d31cb5254083f025e9231e22960e7e14";
-    let url= acaoUrl + baseUrl
-    + "?key=" + apiKey
-    + "&apiCode=ProductSearch&productCode" + productCode;
-    
+    let url = baseUrl
+        + "?key=" + apiKey
+        + "&apiCode=ProductInfo&productCode=" + productCode;
+
+    const data = await axios({
+        url, method: "GET", responseType: 'arraybuffer'
+    })
+    const xml = iconv.decode(data.data, "euc-kr");
+    const productJSON = JSON.parse(convert.xml2json(xml, { compact: true, spaces: 4 }));
+    const product = productJSON.ProductInfoResponse.Product;
+    //console.log(result["ProductInfoResponse"]);
     //res.writeHead("200", {"Content-Type":"text/html;"});
-    console.log("---");
-    
-    //res.send({productCode: productCode}); //******** */
-    res.render("productInfo", {title:"express"});
-    //fs.createReadStream("./public/productInfo.html").pipe(res);
+    console.log("product 생성 완료");
+
+    res.render("productInfo", product);
+
 });
 
 //실행 될 일 Xx
-// router.route("/products").get(function (req, res) {
-//     console.log('/products 호출됨.');
-// });
+/* router.route("/products").get(function (req, res) {
+    console.log('/products 호출됨.');
+}); */
 
-
-let errorHandler = expressErrorHandler({
+//error page 설정
+/* let errorHandler = expressErrorHandler({
     static: {
-      '404': './public/404.html'
+        '404': './public/views/404.html'
     }
 });
 
-app.use( expressErrorHandler.httpError(404) );
-app.use( errorHandler );
+app.use(expressErrorHandler.httpError(404));
+app.use(errorHandler); */
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
---
+++ public/js/products.js
@@ -0,0 +1,76 @@
+const acaoUrl = "https://cors-anywhere.herokuapp.com/";
+const baseUrl = "http://openapi.11st.co.kr/openapi/OpenApiService.tmall";
+const apiKey = "d31cb5254083f025e9231e22960e7e14";
+let url;
+let searchKeyword = "";
+let body = [];
+
+$(document).ready(function () {
+    console.log("ready");
+    $('#btnSearch').click(function () {
+        onClickSearch();
+    });
+});
+
+/* function setDetailUrl(productCode){
+    return "/products/"+productCode;
+} */
+
+function onClickSearch(){
+    searchKeyword = document.getElementById("searchKeyword").value;
+    //console.log(searchKeyword);
+    body = [];
+    url = acaoUrl + baseUrl
+        + "?key=" + apiKey
+        + "&apiCode=ProductSearch&keyword=" + searchKeyword
+        + "&pageSize=8";
+    let html = "";
+
+    $.ajax({
+        method: "GET",
+        datatype: "xml",
+        url: url,
+        success: function (xml) {
+            // let productCount = $(xml).find("Product").Count;
+            $(xml).find("Product").each(function () {
+                let options={
+                    productCode: $(this).find("ProductCode").text(),
+                    detailPageUrl: $(this).find("DetailPageUrl").text(),
+                    productName: $(this).find("ProductName").text(),
+                    productImg:  $(this).find("ProductImage").text(),
+                    productPrice: $(this).find("ProductPrice").text(),
+                    seller: $(this).find("Seller").text(),
+                    buySatisfy: $(this).find("BuySatisfy").text(),
+                    rating: $(this).find("Rating").text(),
+                    reviewCount: $(this).find("ReviewCount").text()
+                }
+                body.push(options);
+            });
+
+            html += "<div class='row' align='center'>";
+            
+            body.forEach(function(item){
+                url = acaoUrl + baseUrl
+                + "?key=" + apiKey
+                + "&apiCode=ProductSearch&productCode="+item["productCode"];
+                //console.log(url);
+                //console.log(item["detailPageUrl"]);
+                //console.log("js:"+item["productCode"]);
+                //console.log(item["rating"] +", " + item["reviewCount"] +","+item["rating"] / item["reviewCount"]);
+                html += "<div class='col-md-4'>"
+                    + "<p><strong>"+ item["productName"] + "</strong>"
+                    + "<br><img src="+item["productImg"]+" alt='상품 이미지'>"
+                    + "<p>" + item["productPrice"]+"원"
+                    + "<p>판매자 정보:" + item["seller"]
+                    //+ "<p>평점:" + item["rating"] / item["reviewCount"]
+                    + "<p>평점:" + item["buySatisfy"]
+                    + "<br>"
+                    +"<input type='button' value='상세설명' class='btn btn-secondary btn-sm' onClick='location.href=`products/"+
+                      item["productCode"] +"`'/>"
+                    + "</div>";
+            });
+            html +="</div>";
+            $("#productList").html(html);
+        }
+    });
+}
